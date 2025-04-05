541a8e08acc383cf34a52870149bf3f15c355eb2
anish <anishpanda3@gmail.com>
2025-04-05 22:21:17 +0530

    done

--- api/index.js
+++ api/index.js
@@ -2,29 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const jobRoutes = require("../routes/Job.Routes");
-const serverless = require("serverless-http"); 
+const jobRoutes = require("../routes/Job.Routes"); 
 
 const app = express();
-
 
 app.use(cors());
 app.use(bodyParser.json());
 
-
 const mongoURI = 'mongodb://localhost:27017/cyber';
-
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-
 app.get('/', (req, res) => {
   res.send('Backend server is running!');
 });
 
+
 app.use('/api/jobs', jobRoutes);
 
-
-module.exports.handler = serverless(app);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
